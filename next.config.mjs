ddd53ce004cd33cbe693556d624b8c791f61eba2
kbpolra <kbpolra@myseneca.ca>
2024-12-05 00:19:37 -0500

    updated next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: "https://user-api-neon-five.vercel.app/api/user",
+  },
 };
 
 export default nextConfig;
